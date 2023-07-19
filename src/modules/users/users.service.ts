ee935dd4ba85892c9ca09530e4146dbf0baab533
Ericles-Miller <59926475+Ericles-Miller@users.noreply.github.com>
2023-07-19 09:34:59 -0300

    create user completed

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { UserDto } from './users.dto';
+import { PrismaService } from 'src/database/Prisma.client';
 
 @Injectable()
 export class UsersService {
-  async create({ avatar, email, name, createdAt }: UserDto) {}
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: UserDto) {
+    const emailAlreadyExists = await this.prisma.user.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (emailAlreadyExists) {
+      throw new Error('email of user already exits!');
+    }
+
+    const user = await this.prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
 }
