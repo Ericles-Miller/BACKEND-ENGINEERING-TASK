fc9a6b21b5ecc692f308dd83951bb24457ec16d3
Ericles Miller <ericlesmiller15@gmail.com>
2023-08-09 21:08:07 -0300

    docker image

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,7 +6,7 @@ import { Response } from 'express';
 import { Express } from 'express'
 import { SendMailProducerService } from 'src/jobs/SendMailProducerService';
 
-@Controller('users')
+@Controller('/users')
 export class UsersController {
   constructor(
     private sendMailService: SendMailProducerService,
